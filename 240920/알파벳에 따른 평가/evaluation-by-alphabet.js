0bcb9d47b213a988d912f60b5e45a23e944296ae
raon37910 <raon37910@gmail.com>
2024-09-20 02:31:30 +0900

    Title: 알파벳에 따른 평가, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240920/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,14 @@
+let c = require('fs').readFileSync(0).toString()
+if(c === 'S') {
+    console.log('Superior')
+} else if(c === 'A') {
+    console.log('Excellent')
+} else if (c === 'B') {
+    console.log('Good')
+} else if (c ==='C') {
+    console.log('Usually')
+} else if (c === 'D') {
+    console.log('Effort')
+} else {
+    console.log('Failure')
+}
