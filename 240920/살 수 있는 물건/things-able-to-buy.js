814b692998dc974527ceea1bb55f4499ceb29aaf
raon37910 <raon37910@gmail.com>
2024-09-20 02:26:30 +0900

    Title: 살 수 있는 물건, Time: 185ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240920/살 수 있는 물건/things-able-to-buy.js
@@ -0,0 +1,10 @@
+let n = require('fs').readFileSync(0).toString()
+n = Number(n)
+
+if(n >= 3000) {
+    console.log('book')
+} else if(n >= 1000) {
+    console.log('mask')
+} else {
+    console.log('no')
+}
