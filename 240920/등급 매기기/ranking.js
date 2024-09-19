4c0279004ea433243a2b2617d0992c16cec7a471
raon37910 <raon37910@gmail.com>
2024-09-20 02:29:29 +0900

    Title: 등급 매기기, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240920/등급 매기기/ranking.js
@@ -0,0 +1,14 @@
+let n = require('fs').readFileSync(0).toString()
+n = Number(n)
+
+if(n >= 90) {
+    console.log('A')
+} else if (n >= 80) {
+    console.log('B')
+} else if (n >= 70) {
+    console.log('C')
+} else if(n >= 60) {
+    console.log('D')
+} else {
+    console.log('F')
+}
