85b7534da53ff88c6a84fa7da16549f3694d9263
raon37910 <raon37910@gmail.com>
2024-09-20 02:28:12 +0900

    Title: 출석 부르기, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240920/출석 부르기/calling-attendance.js
@@ -0,0 +1,12 @@
+let n = require('fs').readFileSync(0).toString()
+n = Number(n)
+
+if(n === 1) {
+    console.log('John')
+} else if(n === 2) {
+    console.log('Tom')
+} else if(n === 3) {
+    console.log('Paul')
+} else {
+    console.log('Vacancy')
+}
