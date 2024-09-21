dda6e831e8cd3244a1f5ec942b6108d3a185dd57
raon37910 <raon37910@gmail.com>
2024-09-21 21:01:00 +0900

    Title: 일 수 구하기, Time: 247ms, Memory: 14MB, Status: Passed - CodeTree

--- 240921/일 수 구하기/number-of-days-in-month.js
+++ 240921/일 수 구하기/number-of-days-in-month.js
@@ -6,8 +6,8 @@ let n = Number(fs.readFileSync(0).toString().trim())
 
 if(n == 2) {
     console.log(28)
-} else if(n % 2 == 1 || n == 8) {
+} else if((n < 8 && n % 2 == 1) || (n >= 8 && n % 2 == 0)) {
     console.log(31)
-} else {
+} else if((n < 8 && n % 2 == 0) || (n >= 8 && n % 2 == 1)) {
     console.log(30)
 }
