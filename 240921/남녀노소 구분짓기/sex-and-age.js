15a8beaf2625c6d21cf77079ae0510471c98c2e5
raon37910 <raon37910@gmail.com>
2024-09-21 20:46:28 +0900

    Title: 남녀노소 구분짓기, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240921/남녀노소 구분짓기/sex-and-age.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+let [gender, age] = fs.readFileSync(0).toString().split('\n')
+
+// 남자 = 0, 여자 = 1
+gender = Number(gender)
+age = Number(age)
+
+if (gender === 0) {
+    if(age >= 19) {
+        console.log('MAN')
+    } else {
+        console.log('BOY')
+    }
+} else {
+    if(gender >= 19) {
+        console.log('WOMAN')
+    } else {
+        console.log('GIRL')
+    }
+}
