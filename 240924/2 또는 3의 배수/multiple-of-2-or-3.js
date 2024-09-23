cb0809093445aa9d0c1a5ccc18aec02067f65297
raon37910 <raon37910@gmail.com>
2024-09-24 03:33:39 +0900

    Title: 2 또는 3의 배수, Time: 169ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240924/2 또는 3의 배수/multiple-of-2-or-3.js
@@ -0,0 +1,13 @@
+let n = require('fs').readFileSync(0).toString().trim()
+n = Number(n)
+let result = ''
+
+for(let i = 1; i <= n; i++) {
+    if(i % 2 == 0 || i % 3 == 0) {
+        result += '1 '
+    } else {
+        result += '0 '
+    }
+}
+
+console.log(result)
