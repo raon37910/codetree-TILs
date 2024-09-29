8bd7b2e7e86067e00f22614ec0ac06ca228cb095
raon37910 <raon37910@gmail.com>
2024-09-29 20:13:19 +0900

    Title: 별표 출력하기 13, Time: 166ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240929/별표 출력하기 13/print-start-13.js
@@ -0,0 +1,33 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+
+for(let i = 1; i <= n; i++) {
+    if(i % 2 === 1) {
+        let line = ''
+        for (let j = 1; j <= n - (i-1) / 2; j++){
+            line += '* '
+        }
+        console.log(line)
+    } else {
+        let line = ''
+        for (let j = 1; j <= (i-1) / 2 + 1; j++){
+            line += '* '
+        }
+        console.log(line)
+    }
+}
+
+for(let i = n; i >= 1; i--) {
+    if(i % 2 === 1) {
+        let line = ''
+        for (let j = 1; j <= n - (i-1) / 2; j++){
+            line += '* '
+        }
+        console.log(line)
+    } else {
+        let line = ''
+        for (let j = 1; j <= (i-1) / 2 + 1; j++){
+            line += '* '
+        }
+        console.log(line)
+    }
+}
