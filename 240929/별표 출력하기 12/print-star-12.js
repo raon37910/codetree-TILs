3667585230e1c3e56d8fdbca45ccf78f02ef8abf
raon37910 <raon37910@gmail.com>
2024-09-29 20:49:15 +0900

    Title: 별표 출력하기 12, Time: 179ms, Memory: 26MB, Status: Passed - CodeTree

--- 240929/별표 출력하기 12/print-star-12.js
+++ 240929/별표 출력하기 12/print-star-12.js
@@ -4,10 +4,12 @@ for(let i = 1; i <= n; i++) {
     let line = ''
     for(let j = 1; j <= n; j++) {
         if(j % 2 === 0) {
-            if(j <= i) {
+            if(i === 1) {
                 line += '* '
-            } else {
+            } else if(j < i){
                 line += '  '
+            } else {
+                line += '* '
             }
         } else {
             if(i === 1) {
