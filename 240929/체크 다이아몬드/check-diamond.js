032593b0ce168e168f4f3ad7c8d0886a7e67d597
raon37910 <raon37910@gmail.com>
2024-09-29 14:55:52 +0900

    Title: 체크 다이아몬드, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240929/체크 다이아몬드/check-diamond.js
@@ -0,0 +1,23 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+
+for(let i = 1; i <= n; i++) {
+    let line = ''
+    for(let j = 1; j <= n-i; j++) {
+        line += ' '
+    }
+    for(let j = 1; j <= i; j++) {
+        line += '* '
+    }
+    console.log(line)
+}
+
+for(let i = n-1; i >= 1; i--) {
+    let line = ''
+    for(let j = 1; j <= n-i; j++) {
+        line += ' '
+    }
+    for(let j = 1; j <= i; j++) {
+        line += '* '
+    }
+    console.log(line)
+}
