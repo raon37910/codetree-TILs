269193e6e5e6bf9df1a0b722f505adc0cc86ed78
raon37910 <raon37910@gmail.com>
2024-10-01 15:08:33 +0900

    Title: 알파벳 출력하기, Time: 229ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241001/알파벳 출력하기/print-alphabet.js
@@ -0,0 +1,10 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+let code = 'A'.charCodeAt()
+
+for(let i = 1; i <= n; i++) {
+    let line = ''
+    for(let j = 1; j <= i; j++) {
+        line += String.fromCharCode(code++)
+    }
+    console.log(line)
+}
