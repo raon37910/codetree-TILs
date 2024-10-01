51b07c0908b5581021be47885747ef4e8eede7d9
raon37910 <raon37910@gmail.com>
2024-10-01 12:51:47 +0900

    Title: 정사각형 출력, Time: 232ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241001/정사각형 출력/print-square.js
@@ -0,0 +1,10 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+let cnt = 1
+
+for(let i = 1; i <= n; i++) {
+    let line = ''
+    for(let j = 1; j <= n; j++) {
+        line += `${cnt++} `
+    }
+    console.log(line)
+}
