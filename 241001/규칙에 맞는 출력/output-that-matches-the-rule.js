b422770c062c3f8db63305075b6a5a766df49ac0
raon37910 <raon37910@gmail.com>
2024-10-01 14:51:55 +0900

    Title: 규칙에 맞는 출력, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241001/규칙에 맞는 출력/output-that-matches-the-rule.js
@@ -0,0 +1,9 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+
+for(let i = 1; i <= n; i++) {
+    let line = ''
+    for(let j = 1; j <= i; j++) {
+        line += `${n-i+j} `
+    }
+    console.log(line)
+}
