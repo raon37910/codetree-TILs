30eadb0fa3b512ea7cd89360805f710b9d21db39
raon37910 <raon37910@gmail.com>
2024-10-01 13:38:06 +0900

    Title: 정상적인 구구단, Time: 317ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241001/정상적인 구구단/normal-multiple.js
@@ -0,0 +1,12 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+
+for(let i = 1; i <= n; i++) {
+    let line = ''
+    for(let j = 1; j <= n; j++) {
+        line += `${i} * ${j} = ${i*j}`
+        if(j !== n) {
+            line += ', '
+        }
+    }
+    console.log(line)
+}
