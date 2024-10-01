84142de3641117fad05ea1eca826ef38ed2dacb2
raon37910 <raon37910@gmail.com>
2024-10-01 15:12:30 +0900

    Title: 알파벳 출력하기 2, Time: 250ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241001/알파벳 출력하기 2/print-alphabet-2.js
@@ -0,0 +1,15 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+let code = 'A'.charCodeAt()
+
+for(let i = 1; i <= n; i++) {
+    let line = ''
+    for(let j = 1; j < i; j++) {
+        line += '  '
+    }
+
+    for(let j = 1; j <= n-i+1; j++) {
+        line += `${String.fromCharCode(code++)} `
+        if(code > 'Z'.charCodeAt()) code = 'A'.charCodeAt()
+    }
+    console.log(line)
+}
