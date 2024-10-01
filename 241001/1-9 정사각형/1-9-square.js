178b923a298c29403c3427ae6e64a7dd2a04e362
raon37910 <raon37910@gmail.com>
2024-10-01 12:53:24 +0900

    Title: 1-9 정사각형, Time: 232ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241001/1-9 정사각형/1-9-square.js
@@ -0,0 +1,13 @@
+let n = Number(require('fs').readFileSync(0).toString().trim())
+let cnt = 1
+
+for(let i = 1; i <= n; i++) {
+    let line = ''
+    for(let j = 1; j <= n; j++) {
+        line += `${cnt++}`
+        if(cnt >= 10) {
+            cnt = 1
+        }
+    }
+    console.log(line)
+}
