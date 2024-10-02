877b73e750a34f93f5c999d11df2d7816a7f9d18
raon37910 <raon37910@gmail.com>
2024-10-02 13:40:27 +0900

    Title: 특정 위치의 문자, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241002/특정 위치의 문자/char-in-specific-location.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync(0).toString()
+let arr = ['L', 'E', 'B', 'R', 'O', 'S']
+
+let index = -1
+
+for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === input) {
+        index = i
+    }
+}
+
+console.log(index === -1 ? 'None' : index)
