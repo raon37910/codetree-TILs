c2aa7de51cee60e61cf93141515467b1182e1604
raon37910 <raon37910@gmail.com>
2024-10-02 13:03:04 +0900

    Title: 십의 자리 숫자, Time: 223ms, Memory: 47MB, Status: Passed - CodeTree

--- 241002/십의 자리 숫자/number-of-tens-digit.js
+++ 241002/십의 자리 숫자/number-of-tens-digit.js
@@ -2,6 +2,7 @@ let  inputs = require('fs').readFileSync(0).toString().trim().split(' ').map(Num
 let count = Array(10).fill(0)
 
 for(let input of inputs) {
+    if(input === 0) break
     if(input < 10) continue
     let num = parseInt(input / 10)
     count[num]++
