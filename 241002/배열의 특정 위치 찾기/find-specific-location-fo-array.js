8de50f51c171101943894882c3437fb2e782f16a
raon37910 <raon37910@gmail.com>
2024-10-02 12:06:59 +0900

    Title: 배열의 특정 위치 찾기, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

--- 241002/배열의 특정 위치 찾기/find-specific-location-fo-array.js
+++ 241002/배열의 특정 위치 찾기/find-specific-location-fo-array.js
@@ -7,10 +7,10 @@ let cntThree = 0
 for(let i = 0; i < arr.length; i++) {
     if((i + 1) % 2 === 0) {
         sumOdd += arr[i]
-    } else if ((i + 1) % 3 === 0) {
+    } 
+    if ((i + 1) % 3 === 0) {
         sumThree += arr[i]
         cntThree++
-        console.log(arr[i])
     }
 }
 let avg = sumThree / cntThree
