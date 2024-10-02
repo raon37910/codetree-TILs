593b38ec67d97911d09a182190d4cadddd88feb6
raon37910 <raon37910@gmail.com>
2024-10-02 11:54:44 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 190ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241002/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -0,0 +1,18 @@
+function checkPassExam(scores) {
+    return scores.reduce((a, b) => a + b, 0) >= 240
+}
+
+let students = require('fs').readFileSync(0).toString().split('\n')
+students = students.slice(1)
+let cnt = 0
+
+for(let scores of students) {
+    scores = scores.split(' ').map(Number)
+    if(checkPassExam(scores)) {
+        console.log('pass')
+        cnt++
+    } else {
+        console.log('fail')
+    }
+}
+console.log(cnt)
