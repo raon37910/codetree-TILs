5893d3d25fa7b5d2a42fa5addad8fd7c07b0bc55
raon37910 <raon37910@gmail.com>
2024-10-03 10:33:56 +0900

    Title: 대문자로 바꾸기, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

--- 241003/대문자로 바꾸기/change-to-capital.js
+++ 241003/대문자로 바꾸기/change-to-capital.js
@@ -2,5 +2,6 @@
 let inputs = require('fs').readFileSync(0).toString().trim().split('\n')
 
 for(let row of inputs) {
-    console.log(row.split(' ').map(a => String.fromCharCode(a.charCodeAt(0) - 32)).join(' '))
+    let [elem1, elem2, elem3] = row.split(' ').map(a => String.fromCharCode(a.charCodeAt(0) - 32))
+    console.log(`${elem1} ${elem2} ${elem3}`)
 }
