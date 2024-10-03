5ab106358fa75d2b180a15da685b5dc82fc94572
raon37910 <raon37910@gmail.com>
2024-10-03 15:12:13 +0900

    Title: 격자 모양 출력하기, Time: 199ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241003/격자 모양 출력하기/print-grid-shape.js
@@ -0,0 +1,16 @@
+let input = require('fs').readFileSync(0).toString().trim().split('\n')
+let [n, m] = input[0].split(' ').map(Number)
+let arr = Array(n+1).fill(0).map(_ => Array(n+1).fill(0))
+
+for(let i = 1; i <= m; i++) {
+    let [r, c] = input[i].split(' ').map(Number)
+    arr[r][c] = r * c
+}
+
+for(let i = 1; i <= n; i++) {
+    let result = ''
+    for(let j = 1; j <= n; j++) {
+        result += `${arr[i][j]} `
+    }
+    console.log(result)
+}
