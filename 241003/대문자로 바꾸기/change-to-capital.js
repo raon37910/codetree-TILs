df7eff07fd90b9cb6b61228aa9c0b803336ce008
raon37910 <raon37910@gmail.com>
2024-10-03 10:32:44 +0900

    Title: 대문자로 바꾸기, Time: 181ms, Memory: 22MB, Status: Wrong Answer - CodeTree

---
+++ 241003/대문자로 바꾸기/change-to-capital.js
@@ -0,0 +1,6 @@
+// 소문자 코드에서 -32 하면 대문자 됨
+let inputs = require('fs').readFileSync(0).toString().trim().split('\n')
+
+for(let row of inputs) {
+    console.log(row.split(' ').map(a => String.fromCharCode(a.charCodeAt(0) - 32)).join(' '))
+}
