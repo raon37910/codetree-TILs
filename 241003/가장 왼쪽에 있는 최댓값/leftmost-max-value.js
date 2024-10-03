9bb549a6f8a47225095e74ecdd098258d4851d38
raon37910 <raon37910@gmail.com>
2024-10-03 10:07:46 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 231ms, Memory: 18MB, Status: Passed - CodeTree

--- 241003/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
+++ 241003/가장 왼쪽에 있는 최댓값/leftmost-max-value.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
 let [n, arr] = fs.readFileSync(0).toString().trim().split('\n')
 arr = arr.split(' ').map(Number)
+// arr= [1 ,1 ,2 ,2 ,3 ,4 ,4 ,5 ,5 ,7 ,8 ,3 ,2 ,5]
 let answers = []
-
+// let cnt = 0
 while(true) {
     let maxIndex = 0
-    let lastIndex = answers.length === 0 ? arr.length : answers[0]-1
+    let lastIndex = answers.length === 0 ? arr.length : answers[answers.length-1]-1
 
     for(let i = 1; i < lastIndex; i++) {
         if(arr[maxIndex] < arr[i]) {
@@ -14,7 +15,9 @@ while(true) {
     }
 
     answers.push(maxIndex+1)
+    // cnt++
     if(maxIndex + 1 === 1) break
+    // if(cnt === 4) break
 }
 
 console.log(answers.join(' '))
