bffff54ac08550ace2b34003835d19018967ed32
raon37910 <raon37910@gmail.com>
2024-10-03 15:07:45 +0900

    Title: 격자에 점 그리기, Time: 164ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 241003/격자에 점 그리기/draw-points-on-grid.js
@@ -0,0 +1,18 @@
+let input = require('fs').readFileSync(0).toString().trim().split('\n')
+let [n, m] = input[0].split(' ').map(Number)
+let arr = Array(n+1).fill(0).map(_ => Array(n+1).fill(0))
+let num = 1
+
+for(let i = 1; i <= m; i++) {
+    let [r, c] = input[i].split(' ').map(Number)
+    arr[r][c] = num
+    num++
+}
+
+for(let i = 1; i <= n; i++) {
+    let result = ''
+    for(let j = 1; j <= n; j++) {
+        result += `${arr[i][j]} `
+    }
+    console.log(result)
+}
