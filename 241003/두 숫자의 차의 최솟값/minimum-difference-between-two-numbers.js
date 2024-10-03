e244246a1985a0685853c78ce053da8446a5b8ef
raon37910 <raon37910@gmail.com>
2024-10-03 10:21:18 +0900

    Title: 두 숫자의 차의 최솟값, Time: 160ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241003/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.js
@@ -0,0 +1,16 @@
+let [n, arr] = require('fs').readFileSync(0).toString().split('\n')
+arr = arr.split(' ').map(Number)
+
+let difMin = Number.MAX_SAFE_INTEGER
+
+for(let i = 0; i < arr.length-1; i++) {
+    for(let j = i + 1; j < arr.length; j++) {
+        let curDiff = Math.abs(arr[i] - arr[j])
+
+        if(curDiff < difMin) {
+            difMin = curDiff
+        }
+    }
+}
+
+console.log(difMin)
