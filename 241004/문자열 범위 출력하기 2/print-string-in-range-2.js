1459a2f56685aa6e4f8ff48f58d454bb9527f9e8
raon37910 <raon37910@gmail.com>
2024-10-04 09:19:00 +0900

    Title: 문자열 범위 출력하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241004/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 241004/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -2,6 +2,11 @@ let [str, n] = require('fs').readFileSync(0).toString().trim().split('\n')
 n = Number(n)
 let result = ''
 
+if(n >= str.length) {
+    console.log(str.reverse())
+    return
+}
+
 for(let i = str.length-1; i > str.length-1-n; i--) {
     result += str[i]
 }
