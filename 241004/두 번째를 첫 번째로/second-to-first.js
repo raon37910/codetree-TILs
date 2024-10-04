88c5dee02d17bf9be851f588462405993642f683
raon37910 <raon37910@gmail.com>
2024-10-04 11:45:00 +0900

    Title: 두 번째를 첫 번째로, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241004/두 번째를 첫 번째로/second-to-first.js
@@ -0,0 +1,11 @@
+let str = require('fs').readFileSync(0).toString().trim().split('')
+let c1 = str[0]
+let c2 = str[1]
+
+for(let i = 0; i < str.length; i++) {
+    if(str[i] === c2) {
+        str[i] = c1
+    } 
+}
+
+console.log(str.join(''))
