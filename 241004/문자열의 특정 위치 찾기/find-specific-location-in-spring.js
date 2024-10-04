21cac27eb90a96bd4601b3a7968571f70e59143b
raon37910 <raon37910@gmail.com>
2024-10-04 11:46:32 +0900

    Title: 문자열의 특정 위치 찾기, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 241004/문자열의 특정 위치 찾기/find-specific-location-in-spring.js
+++ 241004/문자열의 특정 위치 찾기/find-specific-location-in-spring.js
@@ -1,2 +1,2 @@
-let [str, c] = require('fs').readFileSync(0).toString().split(' ')
+let [str, c] = require('fs').readFileSync(0).toString().trim().split(' ')
 console.log(str.indexOf(c) >= 0 ? str.indexOf(c) : 'No')
