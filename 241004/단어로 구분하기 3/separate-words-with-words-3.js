9d924b0b29f535f701a4cd591f33a03434b9e6eb
raon37910 <raon37910@gmail.com>
2024-10-04 09:09:46 +0900

    Title: 단어로 구분하기 3, Time: 223ms, Memory: 14MB, Status: Passed - CodeTree

--- 241004/단어로 구분하기 3/separate-words-with-words-3.js
+++ 241004/단어로 구분하기 3/separate-words-with-words-3.js
@@ -1,2 +1,2 @@
-let strs = require('fs').readFileSync(0).toString().split(' ').reverse().join('\n')
+let strs = require('fs').readFileSync(0).toString().trim().split(' ').reverse().join('\n')
 console.log(strs)
