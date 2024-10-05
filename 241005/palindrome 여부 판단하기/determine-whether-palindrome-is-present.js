447cf6f01fa9eedfdf9bf1475f533da7525e88ce
raon37910 <raon37910@gmail.com>
2024-10-05 13:08:24 +0900

    Title: palindrome 여부 판단하기, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241005/palindrome 여부 판단하기/determine-whether-palindrome-is-present.js
@@ -0,0 +1,11 @@
+function check(str) {
+    let length = parseInt(str.length / 2)
+    for(let i = 0, j = str.length - 1; i < length; i++, j--) {
+        if(str[i] !== str[j]) return false
+    }
+    return true
+}
+let str = require('fs').readFileSync(0).toString()
+// 길이가 6 -> 0 , 1, 2
+// 길이가 5 -> 0, 1
+console.log(check(str) ? 'Yes' : 'No')
