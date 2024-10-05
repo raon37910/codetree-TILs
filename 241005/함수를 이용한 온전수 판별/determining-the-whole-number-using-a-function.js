c33d4ca578ab837b14fe9916d835b366b304f40e
raon37910 <raon37910@gmail.com>
2024-10-05 12:07:29 +0900

    Title: 함수를 이용한 온전수 판별, Time: 218ms, Memory: 14MB, Status: Passed - CodeTree

--- 241005/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.js
+++ 241005/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.js
@@ -1,7 +1,7 @@
 function check(n) {
     if(n % 2 === 0) return false
     if(n % 10 === 5) return false
-    if(n % 3 === 0 && n % 0 !== 0) return false
+    if(n % 3 === 0 && n % 9 !== 0) return false
     return true
 }
 
