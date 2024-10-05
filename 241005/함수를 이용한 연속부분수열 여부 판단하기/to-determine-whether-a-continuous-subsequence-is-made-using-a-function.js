085dd69e95b97cfa90868c6ff8eba5dd70cf523c
raon37910 <raon37910@gmail.com>
2024-10-05 12:32:37 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 160ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241005/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
@@ -0,0 +1,24 @@
+function check(a, b, start, n2) {
+    for(let i = 0; i < n2; i++) {
+        if(a[i + start] !== b[i]) {
+            return false
+        }
+    }
+    return true
+}
+// 수열 B가 수열 A의 연속 부분 수열인가?!
+let inputs = require('fs').readFileSync(0).toString().trim().split('\n')
+let [n1, n2] = inputs[0].split(' ').map(Number)
+let a = inputs[1].split(' ').map(Number)
+let b = inputs[2].split(' ').map(Number)
+if(n1 < n2) {
+    console.log('No')
+} else {
+    for(let i = 0; i < a.length-n2; i++) {
+        if(check(a, b, i, n2)) {
+            console.log('Yes')
+            return
+        }
+    }
+    console.log('No')
+}
