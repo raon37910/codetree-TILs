7e091b28d8476246ce83218cc2287eb46460041a
raon37910 <raon37910@gmail.com>
2024-10-05 11:40:29 +0900

    Title: 함수를 이용한 소수 판별, Time: 190ms, Memory: 15MB, Status: Passed - CodeTree

--- 241005/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
+++ 241005/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -1,13 +1,11 @@
 function isPrime(n) {
+    if(n === 1) return false
     for(let i = 2; i < n; i++) {
         if(n % i === 0) return false
     }
     return true
 }
 let [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number)
-if(a !== 5 && b !== 19) {
-    console.log(a, b)
-} 
 
 let sum = 0
 
