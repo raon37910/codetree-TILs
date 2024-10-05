b6862936d0e73571f42642b0dbadf204dd0c5079
raon37910 <raon37910@gmail.com>
2024-10-05 12:53:28 +0900

    Title: 그 계절, 그 날, Time: 191ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241005/그 계절, 그 날/that-season-that-day.js
@@ -0,0 +1,47 @@
+function leapYear(year) {
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+function getMaxDayOfMonth(month, year) {
+    // console.log(month)
+    if(month === 2) {
+        return leapYear(year) ? 29 : 28
+    }
+
+    if(month === 4 || month === 6 || month === 9 || month === 11) {
+        return 30
+    }
+
+    return 31
+}
+
+function check(day, year, month) {
+    let maxDay = getMaxDayOfMonth(month, year)
+    // console.log(`${month}월 MAX: ${maxDay}`)
+    if(day <= maxDay) return true
+    else return false
+}
+
+function answer(day, year, month) {
+    let answer = ''
+    // console.log(`check: ${check(day, year, month)}`)
+    if(check(day, year, month)) {
+        if(month === 3 || month === 5 || month === 6) {
+            answer = 'Spring'
+        } else if (month === 6 || month === 7 || month === 8) {
+            answer = 'Summer'
+        } else if (month === 9 || month === 10 || month === 11) {
+            answer = 'Fail'
+        } else if(month === 12 || month === 1 || month === 2) {
+            answer = 'Winter'
+        } else {
+            answer = '-1'
+        }
+    } else {
+        answer = '-1'
+    }
+    return answer
+}
+
+let [y, m, d] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number)
+console.log(answer(d, y, m))
