ed32d9be4e88d978cf506cb8ee13a58dd67128f1
raon37910 <raon37910@gmail.com>
2024-10-05 13:19:31 +0900

    Title: 2개 이상의 알파벳, Time: 196ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241005/2개 이상의 알파벳/more-than-one-alphabet.js
@@ -0,0 +1,9 @@
+function solution(str) {
+    for(let i = 1; i <= str.length; i++) {
+        if(str[i] !== str[i-1]) return true
+    }
+    return false
+}
+
+let str = require('fs').readFileSync(0).toString().trim()
+console.log(solution(str) ? 'Yes' : 'No')
