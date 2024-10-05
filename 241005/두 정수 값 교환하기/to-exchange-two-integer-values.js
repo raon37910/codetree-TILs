420ae97143dba5d8fbc79c309868de85a1261fb6
raon37910 <raon37910@gmail.com>
2024-10-05 13:01:18 +0900

    Title: 두 정수 값 교환하기, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

--- 241005/두 정수 값 교환하기/to-exchange-two-integer-values.js
+++ 241005/두 정수 값 교환하기/to-exchange-two-integer-values.js
@@ -1,8 +1,6 @@
 function swap(a, b) {
     return [b, a]
 }
-let [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number)
-let arr = swap(a, b)
-a = arr[0]
-b = arr[1]
+let [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number);
+[a, b] = swap(a, b);
 console.log(`${a} ${b}`)
