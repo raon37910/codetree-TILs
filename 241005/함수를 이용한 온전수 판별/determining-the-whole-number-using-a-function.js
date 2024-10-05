5892dec77728418163cb6afdedc7f76b01e88b5e
raon37910 <raon37910@gmail.com>
2024-10-05 12:06:35 +0900

    Title: 함수를 이용한 온전수 판별, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241005/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.js
@@ -0,0 +1,15 @@
+function check(n) {
+    if(n % 2 === 0) return false
+    if(n % 10 === 0) return false
+    if(n % 3 === 0 && n % 0 !== 0) return false
+    return true
+}
+
+let [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number)
+let cnt = 0
+
+for(let i = a; i <= b; i++) {
+    if(check(i)) cnt++
+}
+
+console.log(cnt)
