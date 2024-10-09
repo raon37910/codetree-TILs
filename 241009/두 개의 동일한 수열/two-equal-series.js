969b1c351a2c3de52a16b491f871a1fa8461dc76
raon37910 <raon37910@gmail.com>
2024-10-09 11:28:29 +0900

    Title: 두 개의 동일한 수열, Time: 184ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241009/두 개의 동일한 수열/two-equal-series.js
+++ 241009/두 개의 동일한 수열/two-equal-series.js
@@ -16,7 +16,7 @@ let answer = true
 
 for(let i = 0; i < n; i++) {
     if(A[i] !== B[i]) {
-        console.log(A[i], B[i])
+        console.log(i, A[i], B[i])
         answer = false
         break
     }
