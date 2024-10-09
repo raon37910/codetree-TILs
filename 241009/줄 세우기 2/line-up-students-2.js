e0e45858c83df8aaf47fc8b8d0451b734ea1d4d2
raon37910 <raon37910@gmail.com>
2024-10-09 16:20:38 +0900

    Title: 줄 세우기 2, Time: 210ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241009/줄 세우기 2/line-up-students-2.js
@@ -0,0 +1,26 @@
+class Student {
+    constructor(index, tall, weight) {
+        this.index = index
+        this.tall = tall
+        this.weight = weight
+    }
+}
+
+let inputs = require('fs').readFileSync(0).toString().trim().split('\n')
+let n = Number(inputs[0])
+let students = [] 
+
+for(let i = 1; i <= n; i++) {
+    let [tall, weight] = inputs[i].trim().split(' ').map(Number)
+    students.push(new Student(i, tall, weight))
+}
+
+students.sort((a, b) => {
+    if(a.tall !== b.tall) return a.tall - b.tall
+    if(a.weight !== b.weight) return b.weight - a.weight
+    return a.index - b.index
+})
+
+for(let s of students) {
+    console.log(`${s.tall} ${s.weight} ${s.index}`)
+}
