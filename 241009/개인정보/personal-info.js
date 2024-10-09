239ea178ef91a27bfc25b6b6a1723c8591ce3f69
raon37910 <raon37910@gmail.com>
2024-10-09 16:01:03 +0900

    Title: 개인정보, Time: 210ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241009/개인정보/personal-info.js
@@ -0,0 +1,34 @@
+class Student {
+    constructor(name, tall, weight) {
+        this.name = name
+        this.tall = tall
+        this.weight = weight
+    }
+}
+
+let inputs = require('fs').readFileSync(0).toString().split('\n')
+let students = []
+
+for(let input of inputs) {
+    let [name, tall, weight] = input.split(' ')
+    tall = Number(tall)
+    weight = Number(weight)
+    students.push(new Student(name, tall, weight))
+}
+
+console.log('name')
+students.sort((a, b) => {
+    if(a.name < b.name) return -1
+    else return 1
+})
+
+for(let s of students) {
+    console.log(`${s.name} ${s.tall} ${s.weight}`)
+}
+
+console.log('\nheight')
+students.sort((a, b) => b.tall -a.tall)
+
+for(let s of students) {
+    console.log(`${s.name} ${s.tall} ${s.weight}`)
+}
