c3b6b3855b275548d8850e373ca08085db38e608
raon37910 <raon37910@gmail.com>
2024-10-10 15:43:53 +0900

    Title: Date to Date, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241010/Date to Date/date-to-date.js
+++ 241010/Date to Date/date-to-date.js
@@ -9,4 +9,5 @@ function changeToDays(month, day) {
 }
 
 let [m1, d1, m2, d2] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number)
-console.log(changeToDays(m2, d2) - changeToDays(m1, d1))
+let ans = changeToDays(m2, d2) - changeToDays(m1, d1)
+console.log(ans === 0 ? 1 : ans)
