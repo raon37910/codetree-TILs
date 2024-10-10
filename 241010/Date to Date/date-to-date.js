bb3c0b0766653f393b2fbcbfe30fd694d443a79e
raon37910 <raon37910@gmail.com>
2024-10-10 15:44:46 +0900

    Title: Date to Date, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

--- 241010/Date to Date/date-to-date.js
+++ 241010/Date to Date/date-to-date.js
@@ -2,7 +2,7 @@ const daysInMonths = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 function changeToDays(month, day) {
     let ret = 0
-    for(let i = 1; i <= month; i++) {
+    for(let i = 1; i < month; i++) {
         ret += daysInMonths[i]
     }
     return ret + day
@@ -10,4 +10,4 @@ function changeToDays(month, day) {
 
 let [m1, d1, m2, d2] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number)
 let ans = changeToDays(m2, d2) - changeToDays(m1, d1)
-console.log(ans === 0 ? 1 : ans)
+console.log(ans + 1)
